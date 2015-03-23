c26e09177c761896604133e86a73cd3f60545636
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2015-03-23 11:48:44 +0100

    MOAR DEBOUNCE! MOAR!

--- service.py
+++ service.py
@@ -18,7 +18,7 @@ class TimerDialogCallback:
 		self.cb = self.pi.callback(self.gpio, pigpio.FALLING_EDGE, self._pulse)
 	
 	def _pulse(self, gpio, level, tick):
-		if tick - self._last_tick < 100:
+		if tick - self._last_tick < 100000:
 			return
 		self._last_tick = tick;
 		dialog = xbmcgui.DialogProgress()
